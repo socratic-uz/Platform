faf4d40abb6f659600328ac0e358f94aeb334d73
Sardor Safarov <skhsafarov@gmail.com>
2026-09-22 00:16:24 +0500

    feat(platform): support conditional feature module compilation with FEATURE_* flags

--- Shared/Shared/Extensions/SharedAppServiceCollectionExtensions.cs
+++ Shared/Shared/Extensions/SharedAppServiceCollectionExtensions.cs
@@ -6,10 +6,18 @@ using Domain.Interfaces.Pages;
 using Shared;
 using Shared.Services;
 using Smart.Web;
+#if FEATURE_MAP
 using Map;
+#endif
+#if FEATURE_MARKDOWN
 using Markdown.Web;
+#endif
+#if FEATURE_FFMPEG
 using FFmpegProcessor;
+#endif
+#if FEATURE_BIOMETRICS
 using Biometrics.Processor;
+#endif
 
 namespace Apps.Shared.Extensions;
 
@@ -31,19 +39,45 @@ public static class SharedAppServiceCollectionExtensions
         {
             if (_featuresRegistered) return;
 
+#if FEATURE_POS
             FeatureRegistry.Register(new Socratic.POS.POSFeatureModule());
+#endif
+#if FEATURE_KIOSK
             FeatureRegistry.Register(new Socratic.Kiosk.KioskFeatureModule());
+#endif
+#if FEATURE_COMMERCE
             FeatureRegistry.Register(new Socratic.Commerce.CommerceFeatureModule());
+#endif
+#if FEATURE_VISION
             FeatureRegistry.Register(new Socratic.Vision.VisionFeatureModule());
+#endif
+#if FEATURE_CHAT
             FeatureRegistry.Register(new Chat.ChatFeatureModule());
+#endif
+#if FEATURE_LANDING
             FeatureRegistry.Register(new Socratic.Landing.LandingFeatureModule());
+#endif
+#if FEATURE_CHECKOUT
             FeatureRegistry.Register(new Socratic.Checkout.CheckoutFeatureModule());
+#endif
+#if FEATURE_ORDERS
             FeatureRegistry.Register(new Socratic.Orders.OrdersFeatureModule());
+#endif
+#if FEATURE_ORGANIZATION
             FeatureRegistry.Register(new Socratic.Organization.OrganizationFeatureModule());
+#endif
+#if FEATURE_IDENTITY
             FeatureRegistry.Register(new Socratic.Identity.IdentityFeatureModule());
+#endif
+#if FEATURE_MAP
             FeatureRegistry.Register(new Map.MapFeatureModule());
+#endif
+#if FEATURE_QRDESIGNER
             FeatureRegistry.Register(new QrDesigner.QrDesignerFeatureModule());
+#endif
+#if FEATURE_SEATDESIGNER
             FeatureRegistry.Register(new SeatDesigner.SeatDesignerFeatureModule());
+#endif
 
             _featuresRegistered = true;
         }
@@ -59,13 +93,23 @@ public static class SharedAppServiceCollectionExtensions
 
         // 2. Register UI and Design System services
         services.AddSmartWebServices();
+#if FEATURE_MAP
         services.AddMapServices();
+#endif
+#if FEATURE_MARKDOWN
         services.AddMarkdownServices();
+#endif
+#if FEATURE_FFMPEG
         services.AddFFmpegProcessorServices();
+#endif
+#if FEATURE_BIOMETRICS
         services.AddBiometricProcessorServices();
+#endif
 
         // 3. Register Core & Shell services
+#if FEATURE_CHAT
         services.AddScoped<Chat.Abstractions.IChatService, Chat.Services.SignalRChatService>();
+#endif
         services.AddScoped<IDetectorService, RemoteDetectorService>();
         services.AddScoped<ISettingsManager, SettingsManager>();
         services.AddScoped<IPasskeyService, PasskeyService>();
